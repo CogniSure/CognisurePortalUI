b33dc2fe9fda31e89064473c274bec4103108c2c
CSUmarani <117824853+CSUmarani@users.noreply.github.com>
2023-08-10 14:30:18 +0530

    Updated subheader

--- src/app/core/table/table.component.ts
+++ src/app/core/table/table.component.ts
@@ -5,15 +5,54 @@ import { SVGIcon, filePdfIcon, fileExcelIcon } from '@progress/kendo-svg-icons';
 import { process } from '@progress/kendo-data-query';
 import { employees } from './employees';
 import { images } from './images';
+import { InboxIconsService } from 'src/app/services/inboxicons.service';
+
+interface NavItem {
+    title: string;
+    routeLink: string;
+    isPipeline?: boolean; 
+    adress?: string; 
+  }
+
+
 @Component({
     selector: 'generic-table',
     templateUrl: './table.component.html',
     styleUrls: ['./table.component.scss']
   })
+
+
+
+
 export class TableComponent implements OnInit {
+
+
+    navItems = [
+        { title: 'Create Submisson', content: '', icon: 'add_box' },
+        { title: '', content: '', icon: 'refresh' },
+        { title: '', routeLink: '', icon: 'description' },
+        { title: '', routeLink: '/contact', icon: 'filter_alt' },
+        { title: '', routeLink: '', icon: 'settings' },
+        { title: 'All 24', routeLink: '/contact', icon: '' },
+        { title: 'New 4', routeLink: '/contact', icon: '' },
+        { title: 'Pending Review 5', routeLink: '', icon: '', icon1: '' },
+        { title: 'Rush 1', routeLink: '', icon: '', icon1: '' },
+        { title: 'Evelyn Salt', routeLink: '', icon: 'arrow_drop_down', icon1: '' }
+      ];
+      
+     
+      dropdownValues: string[] = [];
+      isToggleOn: boolean = false;
+      dropdownOptions: { label: string; link: string }[] = [];
+      isDataAvailble = false;
+      
+
+
+
+
     @Input() data: any[];
 
-    constructor() {
+    constructor(public inboxiconsService: InboxIconsService) {
     }
 
     @ViewChild(DataBindingDirective) dataBinding: DataBindingDirective | undefined;
@@ -28,6 +67,8 @@ export class TableComponent implements OnInit {
         console.log(JSON.stringify(this.data)); 
         this.gridData = this.data;
         this.gridView = this.data;        
+
+        this.fetchDropdownOptions();
     }
 
     public onFilter(value: Event): void {
@@ -85,4 +126,22 @@ export class TableComponent implements OnInit {
 
          return image[code];
      }
+
+
+
+    
+      fetchDropdownOptions(): void {
+        this.dropdownOptions = this.inboxiconsService.getDropdownOptions();
+      }
+    
+      onSelectOption(option: string): void {
+       
+      }
+    
+      toggleDropdown(): void {
+        this.isToggleOn = !this.isToggleOn;
+      }
+
+
+
 }
