53146f360ba939a4cd56195d1680645aa46ada54
Arabinda Moharana <ArabindaM@cognisure.ai>
2023-08-11 13:52:23 +0530

    Updated

--- src/app/core/table/table.component.ts
+++ src/app/core/table/table.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { DataBindingDirective } from '@progress/kendo-angular-grid';
 import { SVGIcon, filePdfIcon, fileExcelIcon } from '@progress/kendo-svg-icons';
@@ -8,141 +7,434 @@ import { images } from './images';
 import { InboxIconsService } from 'src/app/services/inboxicons.service';
 
 interface NavItem {
-    title: string;
-    routeLink: string;
-    isPipeline?: boolean; 
-    adress?: string; 
-  }
-
+  title: string;
+  routeLink: string;
+  isPipeline?: boolean;
+  adress?: string;
+}
 
 @Component({
-    selector: 'generic-table',
-    templateUrl: './table.component.html',
-    styleUrls: ['./table.component.scss']
-  })
-
-
-
-
+  selector: 'generic-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+})
 export class TableComponent implements OnInit {
-
-
-    navItems = [
-        { title: 'Create Submisson', content: '', icon: 'add_box' },
-        { title: '', content: '', icon: 'refresh' },
-        { title: '', routeLink: '', icon: 'description' },
-        { title: '', routeLink: '/contact', icon: 'filter_alt' },
-        { title: '', routeLink: '', icon: 'settings' },
-        { title: 'All 24', routeLink: '/contact', icon: '' },
-        { title: 'New 4', routeLink: '/contact', icon: '' },
-        { title: 'Pending Review 5', routeLink: '', icon: '', icon1: '' },
-        { title: 'Rush 1', routeLink: '', icon: '', icon1: '' },
-        { title: 'Evelyn Salt', routeLink: '', icon: 'arrow_drop_down', icon1: '' }
-      ];
-      
-     
-      dropdownValues: string[] = [];
-      isToggleOn: boolean = false;
-      dropdownOptions: { label: string; link: string }[] = [];
-      isDataAvailble = false;
-      
-
-
-
-
-    @Input() data: any[];
-    @Input() columns: any;
-
-    constructor(public inboxiconsService: InboxIconsService) {
-    }
-
-    @ViewChild(DataBindingDirective) dataBinding: DataBindingDirective | undefined;
-    public gridData: any[];
-    public gridView!: any[]; 
-
-    public mySelection: string[] = [];
-    public pdfSVG: SVGIcon = filePdfIcon;
-    public excelSVG: SVGIcon = fileExcelIcon;
-
-    public ngOnInit(): void {
-        console.log(JSON.stringify(this.columns)); 
-        this.gridData = this.data;
-        this.gridView = this.data;        
-
-        this.fetchDropdownOptions();
-    }
-
-    public onFilter(value: Event): void {
-        const inputValue = value;
-
-        this.gridView = process(this.gridData, {
-            filter: {
-                logic: 'or',
-                filters: [
-                    {
-                        field: 'full_name',
-                        operator: 'contains',
-                        value: inputValue,
-                    },
-                    {
-                        field: 'job_title',
-                        operator: 'contains',
-                        value: inputValue,
-                    },
-                    {
-                        field: 'budget',
-                        operator: 'contains',
-                        value: inputValue,
-                    },
-                    {
-                        field: 'phone',
-                        operator: 'contains',
-                        value: inputValue,
-                    },
-                    {
-                        field: 'address',
-                        operator: 'contains',
-                        value: inputValue,
-                    },
-                ],
-            },
-        }).data;
-
-        if (this.dataBinding != undefined)
-        {
-        this.dataBinding.skip = 0;
-        }
-    }
-
-     public photoURL(dataItem: { img_id: string; gender: string }): string {
-     const code: string = dataItem.img_id + dataItem.gender;
-        const image: { [Key: string]: string } = images;
-
-         return image[code];
-     }
-
-   public flagURL(dataItem: { country: string }): string {
-         const code: string = dataItem.country;
-         const image: { [Key: string]: string } = images;
-
-         return image[code];
-     }
-
-
-
-    
-      fetchDropdownOptions(): void {
-        this.dropdownOptions = this.inboxiconsService.getDropdownOptions();
+  navItems = [
+    { title: 'Create Submisson', content: '', icon: 'add_box' },
+    { title: '', content: '', icon: 'refresh' },
+    { title: '', routeLink: '', icon: 'description' },
+    { title: '', routeLink: '/contact', icon: 'filter_alt' },
+    { title: '', routeLink: '', icon: 'settings' },
+    { title: 'All 24', routeLink: '/contact', icon: '' },
+    { title: 'New 4', routeLink: '/contact', icon: '' },
+    { title: 'Pending Review 5', routeLink: '', icon: '', icon1: '' },
+    { title: 'Rush 1', routeLink: '', icon: '', icon1: '' },
+    { title: 'Evelyn Salt', routeLink: '', icon: 'arrow_drop_down', icon1: '' },
+  ];
+  customColumns: any[] = [
+    {
+      field: "Id",
+      title: "",
+      width:40,
+      type: "checkbox",
+      filterable : false,
+      sortable:false
+    },
+    {
+      field: "SubmissionID",
+      format: "{0:c}",
+      title: "SubmissionID",
+      type: "link",
+      sortable:true
+    },
+    {
+        field: "AccountName",
+        title: "Account Name",
+        type: "text",
+        sortable:false
+      },
+      {
+        field: "EffectiveDate",
+        format: "{0:c}",
+        title: "Effective Date",
+        type: "text",
+        sortable:true
+      },
+      {
+        field: "Type",
+        format: "{0:c}",
+        title: "Type",
+        type: "text",
+        sortable:true
+      },
+    {
+        field: "AgencyName",
+        title: "Agency Name",
+        type: "text",
+        sortable:false
+      },
+      {
+        field: "LOB",
+        format: "{0:c}",
+        title: "LOB",
+        type: "text",
+        sortable:true
+      },
+      {
+        field: "Priority",
+        format: "{0:c}",
+        title: "Priority",
+        type: "text",
+        sortable:true
+      },
+      {
+        field: "Status",
+        format: "{0:c}",
+        title: "Status",
+        type: "text",
+        sortable:true
+      },
+      {
+        field: "AssignedBy",
+        format: "{0:c}",
+        title: "Assigned By",
+        type: "text",
+        sortable:true
       }
-    
-      onSelectOption(option: string): void {
-       
-      }
-    
-      toggleDropdown(): void {
-        this.isToggleOn = !this.isToggleOn;
-      }
+  ];
+  data1 = [
+    {
+      Id: "1",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "2",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "3",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "4",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "5",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "6",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "7",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "8",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "9",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "10",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "11",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "12",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "13",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "14",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "15",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "16",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "17",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "18",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    },
+    {
+      Id: "19",
+      SubmissionID: "B143-03312302301",
+      AccountName: "Dante Mason",
+      EffectiveDate: "03/01/2023",
+      Type: "New Business",
+      AgencyName: "ACE Insurance",
+      LOB: "Property",
+      Priority: "High",
+      Status : "AutoApproved",
+      AssignedBy: "Auto Assigned",
+    },
+    {
+      Id: "20",
+      SubmissionID: "B143-03312302302",
+      AccountName: "Duke & Duke",
+      EffectiveDate: "04/01/2023",
+      Type: "Renewal",
+      AgencyName: "Welch Ins Agency",
+      LOB: "Property,GL, WC",
+      Priority: "Low",
+      Status : "ExtractionException",
+      AssignedBy: "John Williams",
+    }
+  ];
+  dropdownValues: string[] = [];
+  isToggleOn: boolean = false;
+  dropdownOptions: { label: string; link: string }[] = [];
+  isDataAvailble = false;
 
+  @Input() data:any[] 
 
+  @Input() columns: any;
 
+  constructor(public inboxiconsService: InboxIconsService) {}
+
+  @ViewChild(DataBindingDirective) dataBinding:
+    | DataBindingDirective
+    | undefined;
+  public gridData: any[];
+  public gridView!: any[];
+
+  public mySelection: string[] = [];
+  public pdfSVG: SVGIcon = filePdfIcon;
+  public excelSVG: SVGIcon = fileExcelIcon;
+
+  public ngOnInit(): void {
+    console.log(JSON.stringify(this.columns));
+    this.gridData = this.data;
+    this.gridView = this.data;
+
+    this.fetchDropdownOptions();
+  }
+
+  public onFilter(value: Event): void {
+    const inputValue = value;
+
+    this.gridView = process(this.gridData, {
+      filter: {
+        logic: 'or',
+        filters: [
+          {
+            field: 'full_name',
+            operator: 'contains',
+            value: inputValue,
+          },
+          {
+            field: 'job_title',
+            operator: 'contains',
+            value: inputValue,
+          },
+          {
+            field: 'budget',
+            operator: 'contains',
+            value: inputValue,
+          },
+          {
+            field: 'phone',
+            operator: 'contains',
+            value: inputValue,
+          },
+          {
+            field: 'address',
+            operator: 'contains',
+            value: inputValue,
+          },
+        ],
+      },
+    }).data;
+
+    if (this.dataBinding != undefined) {
+      this.dataBinding.skip = 0;
+    }
+  }
+
+  public photoURL(dataItem: { img_id: string; gender: string }): string {
+    const code: string = dataItem.img_id + dataItem.gender;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
+
+  public flagURL(dataItem: { country: string }): string {
+    const code: string = dataItem.country;
+    const image: { [Key: string]: string } = images;
+
+    return image[code];
+  }
+
+  fetchDropdownOptions(): void {
+    this.dropdownOptions = this.inboxiconsService.getDropdownOptions();
+  }
+
+  onSelectOption(option: string): void {}
+
+  toggleDropdown(): void {
+    this.isToggleOn = !this.isToggleOn;
+  }
 }
