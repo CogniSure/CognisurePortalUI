e06b1365459146412a26cfbf4eda7f6a607d2ffd
CSUmarani <117824853+CSUmarani@users.noreply.github.com>
2023-08-17 23:03:02 +0530

    Updated CSS

--- src/app/inbox/helper/prior-carrier-information/prior-carrier-information.component.ts
+++ src/app/inbox/helper/prior-carrier-information/prior-carrier-information.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GridDataResult, DataStateChangeEvent } from '@progress/kendo-angular-grid';
+import { process, State } from '@progress/kendo-data-query';
 
 @Component({
   selector: 'app-prior-carrier-information',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./prior-carrier-information.component.scss']
 })
 export class PriorCarrierInformationComponent {
+  public data: any[] = [
+    { PolicyNumber: "Business Auto", LineOfBusiness: "Business Auto", Carrier: "Business Auto", EffectiveDate:"Business Auto", ExpirationDate:"Business Auto", Premium:"Business Auto" },
+    { PolicyNumber: "Business Auto", LineOfBusiness: "Business Auto", Carrier: "Business Auto", EffectiveDate:"Business Auto", ExpirationDate:"Business Auto", Premium:"Business Auto" },
+    { PolicyNumber: "Business Auto", LineOfBusiness: "Business Auto", Carrier: "Business Auto", EffectiveDate:"Business Auto", ExpirationDate:"Business Auto", Premium:"Business Auto" },
+    { PolicyNumber: "Business Auto", LineOfBusiness: "Business Auto", Carrier: "Business Auto", EffectiveDate:"Business Auto", ExpirationDate:"Business Auto", Premium:"Business Auto" },
+  ];
+  public gridData: GridDataResult = { data: this.data, total: this.data.length };
+  public state: State = {
+    skip: 0,
+    take: 10
+  };
 
+  constructor() { }
+
+  public dataStateChange(state: DataStateChangeEvent): void {
+    this.state = state;
+    this.gridData = process(this.data, this.state);
+  }
+
+  ngOnInit(): void {
+    this.gridData = process(this.data, this.state);
+  }
 }
