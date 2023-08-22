d2fcd5bdde7e5743d1dc4b26ba19e859a7ca4988
CSUmarani <117824853+CSUmarani@users.noreply.github.com>
2023-08-22 21:13:50 +0530

    Updated CSS

--- src/app/model/sidenav/inbox-nav-data.ts
+++ src/app/model/sidenav/inbox-nav-data.ts
@@ -1,32 +1,38 @@
 export const inboxNavbarData = [
     {
         routeLink: '/inbox/detail/summary',
-        icon: 'fal fa-th-large',
+        // icon: 'fal fa-th-large',
+        imagePath:'/assets/images/Summary.svg',
         label: 'Summary'
     },
     {
         routeLink: '/inbox/detail/application',
-        icon: 'far fa-file-text',
+        // icon: 'far fa-file-text',
+        imagePath:'/assets/images/Application.svg',
         label: 'Application'
     },
     {
         routeLink: '/inbox/detail/accountassesment',
-        icon: 'fal fa-file-invoice',
+        // icon: 'fal fa-file-invoice',
+        imagePath:'/assets/images/Account Assessment.svg',
         label: 'Account Assesment'
     },
     {
         routeLink: '/inbox/detail/exposeranalysis',
-        icon: 'far fa-chart-line',
+        // icon: 'far fa-chart-line',
+        imagePath:'/assets/images/Exposure analysis.svg',
         label: 'Exposer Analysis'
     },
     {
         routeLink: '/inbox/detail/lossanalysis',
-        icon: 'far fa-chart-bar',
+        // icon: 'far fa-chart-bar',
+        imagePath:'/assets/images/Loss Analysis.svg',
         label: 'Loss Analysis'
     },
     {
         routeLink: '/inbox/detail/documentvault',
-        icon: 'fal fa-folders',
+        // icon: 'fal fa-folders',
+        imagePath:'/assets/images/Document Vault.svg',
         label: 'Document Vault'
     }
 ];
