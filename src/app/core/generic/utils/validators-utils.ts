4dcfbbb5ec42bb6f5a2670e6024ac52e2cb41a5e
Arabinda Moharana <ArabindaM@cognisure.ai>
2023-09-12 14:09:55 +0530

    Custom match validator

--- src/app/core/generic/utils/validators-utils.ts
+++ src/app/core/generic/utils/validators-utils.ts
@@ -2,17 +2,19 @@ import { ValidationErrors } from "@angular/forms";
 
 export const ValidatorErrorMessage = (validatorName: string, validatorErrors?: ValidationErrors): string|undefined => {
     let args = messages.get(validatorName)?.validatorErrorsKey?.map(name => validatorErrors?.[name]);
-    return (args) ? stringFormat(messages.get(validatorName)?.message,...args) : messages.get(validatorName)?.message;
+    return (args) ? stringFormat(validatorName,messages.get(validatorName)?.message,...args) : messages.get(validatorName)?.message;
 }
 
 const  messages = new Map<string, {message : string,validatorErrorsKey? : string[]}>([
     ['required',  { message : 'This field is required'} ],
     ['minlength', { message : 'Field must be at least {0} characters long' ,   validatorErrorsKey :['requiredLength']}],
     ['maxlength', { message : 'Field cannot be more than {0} characters long', validatorErrorsKey :['requiredLength']}],
-    ['email',     { message : 'Email must be a valid email address'}],
+    ['email', { message : 'Email must be a valid email address'}],
+    ['pattern', { message : 'Invalid format'}],
+    ['matching', { message : 'Value does not Match'}],
 ]);
 
-function stringFormat(template: string|undefined, ...args: any[]) {
+function stringFormat(validatorName:string,template: string|undefined, ...args: any[]) {
     if(template){
         return template.replace(/{(\d+)}/g, (match, index) => {
         return typeof args[index] !== 'undefined'
--- src/app/core/generic/validation-errors/validation-errors.component.ts
+++ src/app/core/generic/validation-errors/validation-errors.component.ts
@@ -36,6 +36,8 @@ ngOnChanges(changes: SimpleChanges): void {
 // }
 get errorMessage() {  
     for (const validatorName in this.control?.errors) {
+      console.log("ValidatorName")
+      console.log(validatorName)
         if(this.control.touched)
           
           return ValidatorErrorMessage(validatorName, this.control.errors[validatorName]);
